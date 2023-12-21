515160ddfd949bcfd06514c0c5bd1141c16bbc60
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2023-12-20 19:47:02 -0500

    first commit

---
+++ test/shapes.test.js
@@ -0,0 +1,11 @@
+const Shapes = require('../shapes.js');
+
+describe('Shapes', () => {
+    describe ('shape', () => {
+        it('', () => {
+            const shape = new Triangle();
+            shape.setColor("blue");
+            expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+        })
+    })
+})
