9cc386e5de802c5716d60b54f8a5665b4f642033
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2023-12-20 20:39:51 -0500

    creates shape render functions and tests

--- shapes.js
+++ shapes.js
@@ -1,24 +1,27 @@
 class Shape {
     constructor() {
-
+        this.color = "";
+    }
+    setColor(colorChoice) {
+        this.color = colorChoice;
     }
 };
 
 class Triangle extends Shape {
     render() {
-
+        return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`;
     }
 };
 
 class Circle extends Shape {
     render() {
-        
+        return `<rect x="73" y="40" width="160" fill="${this.color}" />`;
     }
 };
 
 class Square extends Shape {
     render() {
-        
+        return `<circle cx="150" cy="115" r="80" fill="${this.color}" />`;
     }
 };
 
--- test/shapes.test.js
+++ test/shapes.test.js
@@ -13,7 +13,7 @@ describe ('Circle', () => {
     it('should render a red circle logo', () => {
         const shape = new Triangle();
         shape.setColor("red");
-        expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+        expect(shape.render()).toEqual('<rect x="73" y="40" width="160" fill="red" />');
     })
 });
 
@@ -21,7 +21,7 @@ describe ('Square', () => {
     it('should render a green square logo', () => {
         const shape = new Triangle();
         shape.setColor("green");
-        expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+        expect(shape.render()).toEqual(`<circle cx="150" cy="115" r="80" fill="green" />`);
     })
 });
 
