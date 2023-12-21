05a4823f39965b5ae1849575b4546fc5c8a9a132
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2023-12-20 21:58:48 -0500

    changes const to let, line 42

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function userPrompts() {
 };
 
 function createSvgFile () {
-    const svg = "";
+    let svg = "";
     svg = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
     svg += "<g>";
     svg += `${answers.shape}`;
