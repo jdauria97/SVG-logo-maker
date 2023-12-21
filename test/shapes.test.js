42b5a2a4754ca3a08399907161335f14e0983c6c
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2023-12-20 20:50:54 -0500

    fixes jest functionality

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -3,7 +3,7 @@ const Shapes = require('../shapes.js');
 
 describe ('Triangle', () => {
     it('should render a blue triangle logo', () => {
-        const shape = new Triangle();
+        const shape = new Shapes.Triangle();
         shape.setColor("blue");
         expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
     })
@@ -11,7 +11,7 @@ describe ('Triangle', () => {
 
 describe ('Circle', () => {
     it('should render a red circle logo', () => {
-        const shape = new Triangle();
+        const shape = new Shapes.Circle();
         shape.setColor("red");
         expect(shape.render()).toEqual('<rect x="73" y="40" width="160" fill="red" />');
     })
@@ -19,7 +19,7 @@ describe ('Circle', () => {
 
 describe ('Square', () => {
     it('should render a green square logo', () => {
-        const shape = new Triangle();
+        const shape = new Shapes.Square();
         shape.setColor("green");
         expect(shape.render()).toEqual(`<circle cx="150" cy="115" r="80" fill="green" />`);
     })
