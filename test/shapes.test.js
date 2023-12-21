e868c08f798649e78e695d0ff0ad5e38164f0c05
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2023-12-20 22:46:44 -0500

    adds comments to test

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,6 +1,7 @@
+// imports shapes choices
 const Shapes = require('../shapes.js');
 
-
+// tests render function of each shape
 describe ('Triangle', () => {
     it('should render a blue triangle logo', () => {
         const shape = new Shapes.Triangle();
@@ -8,7 +9,6 @@ describe ('Triangle', () => {
         expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
     })
 });
-
 describe ('Circle', () => {
     it('should render a red circle logo', () => {
         const shape = new Shapes.Circle();
@@ -16,7 +16,6 @@ describe ('Circle', () => {
         expect(shape.render()).toEqual('<rect x="73" y="40" width="160" fill="red" />');
     })
 });
-
 describe ('Square', () => {
     it('should render a green square logo', () => {
         const shape = new Shapes.Square();
@@ -24,4 +23,3 @@ describe ('Square', () => {
         expect(shape.render()).toEqual(`<circle cx="150" cy="115" r="80" fill="green" />`);
     })
 });
-
