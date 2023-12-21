a8a7bf3b2ab6ee8f6ea4fe91eb318856a55cac40
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2023-12-20 21:46:35 -0500

    adds function to createSvgFile() and creates answers

--- index.js
+++ index.js
@@ -28,8 +28,39 @@ function userPrompts() {
             name: "shapeColor",
         }
     ])
+    .then(answers) => {
+        if(answers.text.length > 3) {
+            console.log("Text length limited to 3 characters.");
+            prompt();
+        } else {
+            writeToFile("logo.svg", answers);
+        }
+    }
 };
 
 function createSvgFile () {
+    const svg = "";
+    svg = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    svg += "<g>";
+    svg += `${answers.shape}`;
 
+    let shapeChoice;
+    if (answers.shape === "Triangle") {
+      shapeChoice = new Triangle();
+      svg += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeColor}"/>`;
+    } else if (answers.shape === "Circle") {
+        shapeChoice = new Circle();
+        svg += `<circle cx="150" cy="115" r="80" fill="${answers.shapeColor}"/>`;        
+    } else {
+        shapeChoice = new Square();
+        svg += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeColor}"/>`;
+    }
+
+    svg += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+    svg += "</g>";
+    svg += "</svg>";
+
+    fs.writeFile(fileName, svgString, (err) => {
+        err ? console.log(err) : console.log("Generated logo.svg");
+      });
 };
