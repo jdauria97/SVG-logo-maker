ab28ddb92fac5a58eb28f358df78c1cb7481c4c4
jdauria97 <149337622+jdauria97@users.noreply.github.com>
2023-12-20 20:05:29 -0500

    creates test templates for shapes

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,11 +1,27 @@
 const Shapes = require('../shapes.js');
 
-describe('Shapes', () => {
-    describe ('shape', () => {
-        it('should render a blue triangle logo', () => {
-            const shape = new Triangle();
-            shape.setColor("blue");
-            expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
-        })
+
+describe ('Triangle', () => {
+    it('should render a blue triangle logo', () => {
+        const shape = new Triangle();
+        shape.setColor("blue");
+        expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
     })
-})
+});
+
+describe ('Circle', () => {
+    it('should render a red circle logo', () => {
+        const shape = new Triangle();
+        shape.setColor("red");
+        expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+    })
+});
+
+describe ('Square', () => {
+    it('should render a green square logo', () => {
+        const shape = new Triangle();
+        shape.setColor("green");
+        expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+    })
+});
+
